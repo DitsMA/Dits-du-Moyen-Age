b693ffdd182546d88d5faea25792ced007e807cb
asalamon <anne.salamon@gmail.com>
2019-09-18 19:04:43 -0400

    essai

--- gulpfile.js
+++ gulpfile.js
@@ -16,12 +16,7 @@ function copyresources() {
 }
 
 function copy() {
-  return gulp.src('images/**')
-  .pipe(gulp.dest(target))
-}
-
-function copy() {
-   return gulp.src(['./**/*.html', '!./fragments/**', '!./node_modules/**', '!templet-contenu.html' ])
+   return gulp.src(['./images/**','./**/*.html', '!./fragments/**', '!./node_modules/**', '!templet-contenu.html' ])
       .pipe(fileinclude({
         prefix: '@@',
         basepath: '@file'
