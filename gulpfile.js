aebc161b2d4e44c089df5935c157d45ce3c8d3a7
asalamon <anne.salamon@gmail.com>
2019-09-20 17:42:02 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ function copyresources() {
 }
 
 function copy() {
-   return gulp.src(['./images/**','./**/*.html', './**/*.xml', '!./fragments/**', '!./node_modules/**', '!templet-contenu.html' ])
+   return gulp.src(['./images/**','./**/*.html', './**/*.xsl', './**/*.xml', '!./fragments/**', '!./node_modules/**', '!templet-contenu.html' ])
       .pipe(fileinclude({
         prefix: '@@',
         basepath: '@file'
